0f7890a2e7ec0b98468c48e03309c4977641c036
로건리 <looky1254@naver.com>
2025-09-18 20:56:07 +0900

    feat: 좋아요 수 엔티티 추가 및 리포지토리 추가

--- service/like/src/main/java/rok/board/like/entity/ArticleLikeCount.java
+++ service/like/src/main/java/rok/board/like/entity/ArticleLikeCount.java
@@ -2,6 +2,7 @@ package rok.board.like.entity;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,26 @@ import lombok.ToString;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ArticleLikeCount {
     @Id
-    private Long articleId;
+    private Long articleId; //shard key
+    private Long likeCount;
+    @Version
+    private Long version;
+
+    public static ArticleLikeCount init(Long articleId, Long likeCount) {
+        ArticleLikeCount articleLikeCount = new ArticleLikeCount();
+        articleLikeCount.articleId = articleId;
+        articleLikeCount.likeCount = likeCount;
+        articleLikeCount.version = 0L;
+        return articleLikeCount;
+    }
+
+    public void increase(){
+        this.likeCount++;
+    }
+
+    public void decrease(){
+        if(this.likeCount > 0){
+            this.likeCount--;
+        }
+    }
 }
